8c3d9d09a84934a47c7375d94209d1b47a746985
Karol <nowosadkarol@gmail.com>
2017-05-13 19:13:37 +0200

    initialize project

---
+++ src/main/java/pl/karol/LittleshelterApplication.java
@@ -0,0 +1,12 @@
+package pl.karol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleshelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleshelterApplication.class, args);
+	}
+}
