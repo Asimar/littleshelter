49d6f147c034e68d392a78ae4397f31a3ca9d64d
Karol <nowosadkarol@gmail.com>
2017-05-20 23:03:05 +0200

    Make thymeleaf templating work

--- src/main/java/pl/karol/configuration/WebConfig.java
+++ src/main/java/pl/karol/configuration/WebConfig.java
@@ -1,16 +1,9 @@
 package pl.karol.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
  
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-          .addResourceHandler("/webjars/**")
-          .addResourceLocations("/webjars/");
-    }
 }
